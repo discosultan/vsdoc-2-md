3a8fe560ed973bc527fce66603ffccd58cb456f7
Discosultan <j2v@neti.ee>
2016-05-17 23:48:50 +0300

    Setup workflow

---
+++ converter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Converter = {
+    vsdocToMarkdown: function(vsdoc) {
+        return vsdoc;
+    },
+    
+    markdownToHtml: function(markdown) {
+        return markdown;
+    }
+};
